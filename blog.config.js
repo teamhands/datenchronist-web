0c1fd2c9d24f10601279c79901ed7c7ec49a4e16
teamhands <hands@raitis.de>
2023-06-21 21:37:00 +0200

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'datenchronist@hands.team',
   link: 'https://datenchronist.vercel.app',
   description: 'datenchronist',
-  lang: 'de-DE', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Europe/Berlin', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -18,9 +18,9 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: '#',
+  socialLink: 'https://twitter.com/craigaryhart',
   seo: {
-    keywords: ['Team Hands', 'Datenchronist', 'Team'],
+    keywords: ['Team Hands', 'Hands', 'Team'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
